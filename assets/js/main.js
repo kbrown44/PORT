75d1023277c03fbed035502f1879a6609994b44b
Unknown <kbswr444@gmail.com>
2018-02-13 20:15:58 -0500

    Auto Load
    Gallery auto load

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,20 @@
+//******VARIABLES******
+
+var images = 22; //Number of Images to Be Loaded, start at '0'
+var place = '0'; //Pointer for Gallery
 
 
+//******LOAD IMAGES******
 
 $(document).ready(function(){
-        var dir = "images/thumbnail/"; // folder location
+        var dir = "../images/thumbnail/"; // folder location
         var fileextension = ".jpg"; // image format
         var i = "0";
         
         $(function imageloop(){
-          $("<div  />").attr('class', "picDiv" ).attr('style', "background-image:url(" + dir + i + fileextension + ")" ).attr('onclick', "javascript:firstOpen(" + i + ");").appendTo("#gallery");
-          //$("<img />").attr('src', dir + i + fileextension ).appendTo("#testDiv" + i);
+          $("<div  />").attr('class', "thumb" ).attr('id', "div" + i ).appendTo("#gallery");
+          $("<figure  />").attr('class', "thumb" ).attr('id', "figure" + i ).appendTo("#div" + i);
+          $("<img>").attr('class', "thumb" ).attr('src',  dir + i + fileextension ).appendTo("#figure" + i);
           if (i < images){
             i++;
             imageloop();
